3259248f89904ca6717d1181193a8127d608c700
Pablo <31217215+pdehevia@users.noreply.github.com>
2023-10-25 16:47:26 +0200

    Update ExampleInstrumentedTest.kt

--- sampleApp/src/androidTest/java/com/tonyodev/fetchapp/ExampleInstrumentedTest.kt
+++ sampleApp/src/androidTest/java/com/tonyodev/fetchapp/ExampleInstrumentedTest.kt
@@ -18,7 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
+        val appContext = InstrumentationRegistry.getInstrumentation().getTargetContext()
         assertEquals("com.tonyodev.fetchapp", appContext.packageName)
     }
 }
