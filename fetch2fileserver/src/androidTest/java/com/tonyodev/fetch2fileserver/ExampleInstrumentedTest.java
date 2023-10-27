5c9f8dd2113d6a0862af045a3d1c9bbebc394286
Pablo <31217215+pdehevia@users.noreply.github.com>
2023-10-27 08:26:34 +0200

    Update ExampleInstrumentedTest.java

--- fetch2fileserver/src/androidTest/java/com/tonyodev/fetch2fileserver/ExampleInstrumentedTest.java
+++ fetch2fileserver/src/androidTest/java/com/tonyodev/fetch2fileserver/ExampleInstrumentedTest.java
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
         assertEquals("com.tonyodev.fetch2fileserver.test", appContext.getPackageName());
     }
